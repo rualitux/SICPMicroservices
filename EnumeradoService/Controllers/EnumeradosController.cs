da466eaf803cbc42daf2403cfcb965dfc0e72ff9
rualitux <rualitux@gmail.com>
2023-01-10 01:39:39 -0500

    Updated EnumeradosController with GetEnumeradoById and CreateEnumerado

--- EnumeradoService/Controllers/EnumeradosController.cs
+++ EnumeradoService/Controllers/EnumeradosController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using EnumeradoService.Dtos;
 using EnumeradoService.Interfaces;
+using EnumeradoService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnumeradoService.Controllers
@@ -24,5 +25,28 @@ namespace EnumeradoService.Controllers
             var EnumeradoItem = _repository.GetAllEnumerado();
             return Ok(_mapper.Map<IEnumerable<EnumeradoReadDto>>(EnumeradoItem));
         }
+        [HttpGet("{id}", Name = "GetEnumeradoById")]
+        public ActionResult<EnumeradoReadDto> GetEnumeradoById(int id)
+        {
+            var enumeradoItem = _repository.GetEnumeradobyId(id);
+            if (enumeradoItem != null)
+            {
+                return Ok(_mapper.Map<EnumeradoReadDto>(enumeradoItem));
+            }
+            return NotFound();
+          
+        }
+        [HttpPost]
+        public ActionResult<EnumeradoReadDto> CreateEnumerado(EnumeradoCreateDto enumeradoCreateDto)
+        {
+            var enumeradoModel = _mapper.Map<Enumerado>(enumeradoCreateDto);
+            _repository.CreateEnumerado(enumeradoModel);
+            _repository.Save();
+
+            var enumeradoReadDto = _mapper.Map<EnumeradoReadDto>(enumeradoModel);
+            return CreatedAtRoute(nameof(GetEnumeradoById), new { Id = enumeradoReadDto.Id }, enumeradoReadDto);
+        }
+
     }
+    
 }
